d3afd78d1a2974f040016d29c3fd081525613382
Sergio Gimenez <gimenezsergio123@gmail.com>
2022-09-23 09:43:09 -0300

    Tomamos y guardamos datos de la tarjeta de credito

--- carrito.js
+++ carrito.js
@@ -78,5 +78,17 @@ let cantidadItems = function() {
 
 }
 
+let tarjeta = function(){
+    let tarjeta = {}
+    tarjeta.nombre = document.querySelector("#typeName").value
+    tarjeta.numero = document.querySelector("#typeCardNumber").value
+    tarjeta.exp = document.querySelector("#typeExp").value
+    tarjeta.cvv = document.querySelector("#typeCvv").value
+    
+    
+    localStorage.setItem('tarjeta',JSON.stringify(tarjeta))
+    console.log(tarjeta)
+}
+
 recuperarData()
 
